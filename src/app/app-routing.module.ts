2a2d8f651373081b47cf38a0bd737816f3113e25
dikobrazik <ilnar.khusaenov@gmail.com>
2020-11-14 13:11:56 +0300

    feat(layout): done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './modules/layout/ui/layout/layout.component';
 import { UsersListPage } from './modules/user/ui/pages/user-list/user-list.component';
 
 const routes: Routes = [
-  { path: 'users', component: UsersListPage },
-  { path: 'departments', component: UsersListPage },
-  { path: 'contacts', children: [{ path: 'add', component: UsersListPage }] },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'users', component: UsersListPage },
+      { path: 'departments', component: UsersListPage },
+      {
+        path: 'contacts',
+        children: [{ path: 'add', component: UsersListPage }],
+      },
+    ],
+  },
+  { path: '**', redirectTo: '/users' },
 ];
 
 @NgModule({
