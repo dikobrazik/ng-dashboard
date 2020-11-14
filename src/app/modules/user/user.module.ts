735c5a534e82a4dace7996bbb9816549db84a0ac
dikobrazik <ilnar.khusaenov@gmail.com>
2020-11-14 23:40:37 +0300

    feat(user@ui): checkbox added to users list

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CommonComponentsModules } from '../ud-ui/components/components.module';
 import { UserService } from './domain/services/UserService';
 import { UsersListPage } from './ui/pages/user-list/user-list.component';
 
 @NgModule({
   declarations: [UsersListPage],
-  imports: [CommonModule],
+  imports: [CommonModule, CommonComponentsModules],
   providers: [UserService],
 })
 export class UserModule {}
