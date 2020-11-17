0a47220462d040aa5d8ef84334f8b84a3dd7d8b8
dikobrazik <ilnar.khusaenov@gmail.com>
2020-11-17 12:46:29 +0300

    feat(ud-ui@components): checkbox can work with forms

--- src/app/modules/ud-ui/components/checkbox/index.component.ts
+++ src/app/modules/ud-ui/components/checkbox/index.component.ts
@@ -1,17 +1,45 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, forwardRef, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { uniqueId } from 'lodash';
 
 @Component({
   selector: 'ud-checkbox',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => UDComponentsCheckbox),
+      multi: true,
+    },
+  ],
 })
-export class UDComponentsCheckbox implements OnInit {
+export class UDComponentsCheckbox implements OnInit, ControlValueAccessor {
   id: string = '';
+  checked: boolean = false;
   @Input() name: string = '';
   @Input() label?: string = '';
 
   ngOnInit() {
     this.id = uniqueId('checkbox-');
+  }
+
+  private onChange = (value: boolean) => {};
+  private onTouched = () => {};
+
+  onValueChange($event: any) {
+    this.writeValue($event.target.checked);
+  }
+
+  public writeValue(value: boolean) {
+    this.checked = value;
+    this.onChange(value);
+    this.onTouched();
+  }
+  public registerOnChange(fn: (value: boolean) => void) {
+    this.onChange = fn;
+  }
+  public registerOnTouched(fn: () => void) {
+    this.onTouched = fn;
   }
 }
--- src/app/modules/ud-ui/components/components.module.ts
+++ src/app/modules/ud-ui/components/components.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IconSpriteModule } from 'ng-svg-icon-sprite';
 import { UDComponentsCheckbox } from './checkbox/index.component';
 import { UDComponentsIcon } from './icon/index.component';
 
 @NgModule({
   declarations: [UDComponentsIcon, UDComponentsCheckbox],
-  imports: [IconSpriteModule, CommonModule],
+  imports: [IconSpriteModule, CommonModule, FormsModule, ReactiveFormsModule],
   exports: [UDComponentsIcon, UDComponentsCheckbox],
 })
 export class CommonComponentsModules {}
