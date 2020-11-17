829bbc471cdb0b29757e313e37e19c8e176d29d2
dikobrazik <ilnar.khusaenov@gmail.com>
2020-11-17 22:45:47 +0300

    finalize

--- src/app/modules/user/ui/pages/user-list/user-list.component.ts
+++ src/app/modules/user/ui/pages/user-list/user-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { from, Observable } from 'rxjs';
 import { UserService } from '../../../domain/services/UserService';
 import { User } from 'src/app/modules/user/domain/interfaces/User';
+import { finalize } from 'rxjs/operators';
 
 const headers = [
   'Name',
@@ -28,9 +29,15 @@ export class UsersListPage implements OnInit {
 
   ngOnInit(): void {
     this.isLoading = true;
-    this.userService.load().subscribe((users) => {
-      this.users = users;
-      this.isLoading = false;
-    });
+    this.userService
+      .load()
+      .pipe(
+        finalize(() => {
+          this.isLoading = true;
+        })
+      )
+      .subscribe((users) => {
+        this.users = users;
+      });
   }
 }
