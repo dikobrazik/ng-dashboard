e445e843b50dfc17b0556eafa0b2e2619293f6a9
dikobrazik <ilnar.khusaenov@gmail.com>
2020-11-17 12:47:11 +0300

    feat(contact@ui): contact create form done

--- src/app/modules/contact/contact.module.ts
+++ src/app/modules/contact/contact.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UDFormModule } from '../ud-form/ud-form.module';
 import { CommonComponentsModules } from '../ud-ui/components/components.module';
 import { ContactPagesCreate } from './ui/pages/create/index.component';
 
 @NgModule({
   declarations: [ContactPagesCreate],
   imports: [
+    UDFormModule,
     CommonModule,
     CommonComponentsModules,
     ReactiveFormsModule,
--- src/app/modules/contact/ui/pages/create/index.component.ts
+++ src/app/modules/contact/ui/pages/create/index.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {
+  AbstractControl,
   FormArray,
   FormBuilder,
   FormControl,
   FormGroup,
+  ValidationErrors,
   Validators,
 } from '@angular/forms';
 
@@ -14,19 +16,14 @@ import {
 })
 export class ContactPagesCreate implements OnInit {
   days = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
-  contactForm = this.fb.group({
-    firstName: this.fb.control('', [Validators.minLength(3)]),
-    lastName: this.fb.control('', [Validators.minLength(3)]),
-    role: this.fb.control('', [Validators.required]),
-    password: this.fb.control('', [Validators.required]),
-    repeatPassword: this.fb.control('', [Validators.required]),
-    schedule: this.fb.array(this.days.map(() => this.fb.control(''))),
-  });
-
-  constructor(private fb: FormBuilder) {}
+  contactForm: FormGroup;
 
   get scheduleControls() {
     return (this.contactForm.controls['schedule'] as FormArray).controls;
+  }
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.buildForm();
   }
 
   ngOnInit() {}
@@ -36,6 +33,64 @@ export class ContactPagesCreate implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.contactForm.value);
+    if (!this.contactForm.valid) {
+      return console.log(this.contactForm);
+    }
+    console.log(this.contactForm.valid, this.contactForm.value);
+  }
+
+  private buildForm(): FormGroup {
+    return this.fb.group(
+      {
+        firstName: [
+          null,
+          [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(100),
+          ],
+        ],
+        lastName: [
+          null,
+          [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(100),
+          ],
+        ],
+        role: [null, [Validators.required]],
+        password: [null, [Validators.required, Validators.minLength(6)]],
+        repeatPassword: [null, [Validators.required]],
+        schedule: this.fb.array(
+          this.days.map(() => [null]),
+          this.scheduleValidator
+        ),
+        active: [null],
+      },
+      { validators: [this.passwordsValidator] }
+    );
+  }
+
+  private scheduleValidator(
+    formArray: AbstractControl
+  ): ValidationErrors | null {
+    if (!formArray.value.filter(Boolean).length) {
+      return {
+        minArrayLength: {
+          requiredLength: 1,
+          actualLength: formArray.value.filter(Boolean).length,
+        },
+      };
+    }
+    return null;
+  }
+
+  private passwordsValidator(formGroup: FormGroup): ValidationErrors | null {
+    if (formGroup.value['password'] !== formGroup.value['repeatPassword']) {
+      formGroup.get('repeatPassword')?.setErrors({
+        same: 'password',
+      });
+    }
+    return null;
   }
 }
